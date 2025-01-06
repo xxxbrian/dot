1ebfac181698d5f14d3db1f7daa4e7cfbf24407b
Bojin Li <bojinxx@gmail.com>
2025-01-06 09:21:19 +0800

    feat(script): add lazy.zsh

--- check.sh
+++ check.sh
@@ -17,6 +17,7 @@ cp -r ~/.config/yazi ./config # yazi
 
 # duplicate zshrc file
 cp ~/.zshrc ./home/.zshrc
+cp ~/.lazy.zsh ./home/.lazy.zsh
 
 # duplicate editorconfig file
 cp ~/.editorconfig ./home/.editorconfig
